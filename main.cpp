5e0356bf812fa0d779671c0d08a8cfe51f44e6db
HyhTonHappy <121785342+HyhTonHappy@users.noreply.github.com>
2024-10-19 11:09:55 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ii int
+#define ll long long
+#define mod 1000000007
+//#define FOR(i, n) for(int i = 1; i <= (n); i++)
+#define FOR(i, n) for(int i = 0; i < s.size(); i++)
+#define PI 3.14159265358979323846
+#define INF INT_MAX
+#define MINF INT_MIN
+
+
+int main() {
+ ios_base::sync_with_stdio(false);
+
+
+ return 0;
+}
