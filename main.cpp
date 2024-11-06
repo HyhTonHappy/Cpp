787234d03977e541237350aa91569b524401b459
HyhTonHappy <121785342+HyhTonHappy@users.noreply.github.com>
2024-11-06 20:23:25 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 #define ii int
 #define ll long long
+#define str string
 #define mod 1000000007
-//#define FOR(i, n) for(int i = 1; i <= (n); i++)
-#define FOR(i, n) for(int i = 0; i < s.size(); i++)
 #define PI 3.14159265358979323846
 #define INF INT_MAX
 #define MINF INT_MIN
+//unordered_map <long long, long long> mp;
+//set <long long> st;
 
+
+ll n;
+str s;
+
+void file (){
+    ifstream inp("check.inp");
+    ofstream out("check.out");
+}
+
+void inp_file(){
+    // inp
+}
+
+void out_file(){
+    // out
+}
 
 int main() {
- ios_base::sync_with_stdio(false);
-
-
- return 0;
+   
+    return 0;
 }
